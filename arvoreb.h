458e8d13679077bf65997a76f0a2a4ff6459b4c5
João Victor Jardim <116205943+melanciaboy@users.noreply.github.com>
2023-05-31 10:02:44 -0300

    Arvore B

---
+++ arvoreb.h
@@ -0,0 +1,37 @@
+#ifndef arvoreb_h
+#define arvoreb_h
+#define MM 4
+
+typedef long TipoChave;
+
+typedef struct TipoRegistro {
+    TipoChave Chave;
+} TipoRegistro;
+
+typedef struct TipoPagina* TipoApontador;
+
+typedef struct TipoPagina {
+    short n;
+    TipoRegistro r[MM];
+    TipoApontador p[MM +1];
+} TipoPagina;
+
+//Pesquisa
+void Pesquisa (TipoRegistro, TipoApontador);
+
+//Caminhamento
+void Imprime (TipoApontador);
+
+//Inserção
+void InsereNaPagina (TipoApontador, TipoRegistro, TipoApontador);
+void Ins(TipoRegistro, TipoApontador, short*, TipoRegistro, TipoApontador);
+void Insere(TipoRegistro, TipoApontador);
+
+
+//Remoção
+void Reconstitui (TipoApontador, TipoApontador, int, short*);
+void Antecessor (TipoApontador, int, TipoApontador, short*);
+void Ret(TipoChave, TipoApontador, short*);
+void Retira (TipoChave, TipoApontador);
+
+#endif 
