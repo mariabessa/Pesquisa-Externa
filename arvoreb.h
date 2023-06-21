d762a2d24124c23a01a0a7471931e607387b7f59
gabrielhenrique-123 <gabrielherniquerocha@gmail.com>
2023-06-20 21:07:01 -0300

    Changes in the arvoreb.c e .h

---
+++ arvoreb.h
@@ -0,0 +1,51 @@
+#ifndef arvoreb_h
+#define arvoreb_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MM 4
+#define M 2
+
+typedef long TipoChave;
+
+typedef struct TipoRegistro {
+    TipoChave Chave;
+
+    int chave;   //valor inteiro 
+    long int dado1;  //valor inteiro longo 
+    char dado2[1000];    //cadeia de 1000 caracteres 
+    char dado3[5000];    //cadeia de 5000 caractere 
+} TipoRegistro;
+
+typedef struct TipoPagina* TipoApontador;
+
+typedef struct TipoPagina {
+    short n;
+    TipoRegistro r[MM];
+    TipoApontador p[MM +1];
+} TipoPagina;
+
+//Inicializar
+void Inicializa (TipoApontador); 
+
+
+//Pesquisa
+void Pesquisa (TipoRegistro*, TipoApontador);
+
+//Caminhamento
+void Imprime (TipoApontador);
+
+//Inserção
+void InsereNaPagina (TipoApontador, TipoRegistro, TipoApontador);
+void Ins(TipoRegistro, TipoApontador, short*, TipoRegistro*, TipoApontador*);
+void Insere(TipoRegistro, TipoApontador*);
+
+
+//Remoção
+void Reconstitui (TipoApontador, TipoApontador, int, short*);
+void Antecessor (TipoApontador, int, TipoApontador, short*);
+void Ret(TipoChave, TipoApontador*, short*);
+void Retira (TipoChave, TipoApontador*);
+
+#endif 
---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include "ASI.h"
+#include "criaArq.h"
+#include "arvoreb.h"
+
+
+int main(int argc, char **argv)
+{
+    int opcode;
+    Item itemProcurado;
+    opcode = atoi(argv[1]);
+    itemProcurado.chave = atoi(argv[3]);
+    arquivo();
+    /*  argv[0] = método - acesso sequencial, binario, arvore b, arvore b estrela
+        argv[1] = quantidade de registros
+        argv[2] = situação (ordenado descententemente, ascendentemente ou desordenado aleatoriamente)
+        argv[3] = chave a ser pesquisada
+        argv[4](opcional) = [-P] colocado caso deseja que as chaves de pesquisa dos registros do arquivo considerado sejam apresentadas na tela
+    */
+    FILE *arq;
+    if((arq = fopen("arquivo.bin", "rb")) == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return 0;
+    }
+    //Número de argumentos inválido
+    if (argc!=5 && argc!=6) 
+        printf("Número de argumentos inválido. As entradas são: <método> <quantidade> <situação> <chave> [-P](opcional)\n");
+    // PESQUISA DE ACESSO INDEXADO:
+    else if (opcode == 1)  { 
+        if(acessoSequencialIndexado(&itemProcurado, arq))
+            printf("Item: %d \nDado1: %ld\n", itemProcurado.chave, itemProcurado.dado1);
+        else printf("O livro não está no arquivo\n");
+        return 0;
+    }
+
+    else if (opcode == 3)  {
+        TipoApontador* arvoreb = (TipoApontador*) malloc (sizeof(TipoApontador*));
+        TipoRegistro* registro = (TipoRegistro*) malloc (sizeof(TipoRegistro*));
+        FILE* arquivo;
+
+        arquivo = fopen("arquivo.bin", "rb");
+
+        //Inicialização
+        Inicializa(*arvoreb);
+
+        //Inserção
+        for (int i = 0; i < argv[2]; i++){
+            fscanf("%d %ld %s %s", arquivo);
+            Insere(*registro, arvoreb);
+        }
+
+        //Tipo de ordenação
+
+        //Pesquisa
+        Pesquisa(registro, *arvoreb);
+
+        if (argc==5){ //Usuario digitou -P
+             printf(" ");
+        }
+
+        free(arvoreb);
+        free(registro);   
+    }
+}
